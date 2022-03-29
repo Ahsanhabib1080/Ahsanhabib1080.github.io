fa0fc4e6c1e1ac01f1380921b39ce9b6996915e5
Ahsan Habib <43509085+Ahsanhabib1080@users.noreply.github.com>
2022-03-29 22:11:11 +0600

    Added Javascript
    Jquery Added

---
+++ js/contact.js
@@ -0,0 +1,30 @@
+$('#Submit').click(function(){
+  // Gather Information from the form / input fields and Do something with it.
+  var f_name = $('#firstname').val();
+  var l_name = $('#lastname').val();
+  var email = $('#email').val();
+  var pass = $('#password').val();
+  var message = $('#message').val();
+
+  var fullName = f_name + ' ' + l_name;
+  $('#fullname:text').val(fullName);
+  $('#semail:text').val(email);
+
+  if(f_name==''||email==''||message=='' || l_name==''){
+      alert("Please Fill All Fields");
+    }
+    else{
+      alert("Form Submitted Successfully!");
+
+      $('form').trigger('reset');
+    }
+});
+
+$('#checkbox').click(function(){
+  /// Show password by checkbox
+  if($('#checkbox').is(':checked')){
+    $('#password').attr('type','text');
+  }else{
+    $('#password').attr('type','password');
+  }
+});
